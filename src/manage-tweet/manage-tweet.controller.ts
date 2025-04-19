c7c705a5c5af49b418df4f8c1126be9ef973abc9
afif1731 <afifsdi@gmail.com>
2025-04-19 21:34:34 +0700

    fix: lint error

--- src/manage-tweet/manage-tweet.controller.ts
+++ src/manage-tweet/manage-tweet.controller.ts
@@ -1,11 +1,7 @@
 import { Controller } from '@nestjs/common';
 
-import { ManageTweetService } from './manage-tweet.service';
-
 @Controller('manage-tweet')
 export class ManageTweetController {
-  constructor(private readonly manageTweetService: ManageTweetService) {}
-
   // @Get('sentiment')
   // async getTweetSentiment(@Query('ids') ids: string) {
   //   if (!ids || ids === '')
