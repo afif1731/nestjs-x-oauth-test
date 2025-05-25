2a2be1cd6c564dc9ac2dab92d814cd08e38fdc3e
afif1731 <afifsdi@gmail.com>
2025-05-25 10:15:14 +0700

    feat: faker

--- src/statistic/statistic.service.ts
+++ src/statistic/statistic.service.ts
@@ -4,6 +4,8 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { ErrorResponse } from 'common';
 import { format } from 'date-fns';
+import * as uuid from 'uuid';
+import { fakerID_ID } from '@faker-js/faker';
 
 import { PrismaService } from 'infra/database/prisma/prisma.service';
 
@@ -51,6 +53,17 @@ export class StatisticService {
       };
     });
 
+    if (formattedResult.length < 12) {
+      let fakeData = this.generateFakeTweetData(
+        Number(result[0].year),
+        Number(result[0].month),
+        12 - formattedResult.length,
+      );
+      fakeData.push(...formattedResult);
+
+      return fakeData;
+    }
+
     return formattedResult;
   }
 
@@ -84,6 +97,69 @@ export class StatisticService {
       },
     });
 
+    if (data.length < 20) {
+      let fakeData = this.generateFakeUserData(
+        data[data.length - 1].created_at,
+        20 - data.length,
+      );
+      fakeData.push(...data);
+
+      return fakeData;
+    }
+
     return data;
+  }
+
+  generateFakeTweetData(lastYear: number, lastMonth: number, amount: number) {
+    let result: { month: string; amount: number }[] = [];
+    let year: number = lastYear;
+    let month: number = lastMonth - 1;
+
+    if (month === 0) {
+      year--;
+      month = 12;
+    }
+
+    for (let i = 0; i < amount; i++) {
+      const date = new Date(Number(year), Number(month) - 1);
+      const fakeAmount = Math.ceil(Math.random() * 10000 + 100) % 50000;
+
+      result.push({
+        month: format(date, 'MMMM yyyy'),
+        amount: fakeAmount,
+      });
+
+      month--;
+
+      if (month === 0) {
+        year--;
+        month = 12;
+      }
+    }
+
+    return result.reverse();
+  }
+
+  generateFakeUserData(lastTime: Date, amount: number) {
+    let result: {
+      blocked_user_id: string;
+      blocked_tweet_id: string;
+      blocked_username: string;
+      created_at: Date;
+    }[] = [];
+    let time: Date = new Date(lastTime);
+
+    for (let i = 0; i < amount; i++) {
+      time = new Date(time.getTime() - (Math.ceil(Math.random() * 1000) % 50));
+
+      result.push({
+        blocked_tweet_id: uuid.v4(),
+        blocked_user_id: uuid.v4(),
+        blocked_username: `${fakerID_ID.person.firstName()}${fakerID_ID.person.lastName()}`,
+        created_at: time,
+      });
+    }
+
+    return result.reverse();
   }
 }
